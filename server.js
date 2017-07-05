26a1c9dc16c0005fa19ca5d61b8f7f6aa0ba4e98
Bryan Gonnella <b.gonnella94@gmail.com>
2017-07-05 13:28:29 -0700

    implmented .env file

--- server.js
+++ server.js
@@ -1,34 +1,48 @@
 'use strict'
+const controller = require('./app/controller');
+const env = require('node-env-file');
 const express = require('express');
 const fs = require('fs');
-const app = express();
 const bodyParser = require('body-parser');
-var port = process.env.PORT || 8000;
+const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// Setup lights to be controlled here
-const controller = require('./app/controller');
+// Globals
+var devices = [];
+var port;
+var lights;
 
-var b1 = require('net').Socket();
-var b2 = require('net').Socket();
-var b3 = require('net').Socket();
+/*
+  Main function run when server is started
+*/
+function init() {
+  env('.env'); // parse environment variables from .env file
 
-b1.connect(5577, 'bulb1');
-b2.connect(5577, 'bulb2');
-b3.connect(5577, 'bulb3');
+  port = process.env.PORT;
+  lights = process.env.BULBS.split(' ');
+
+  for (var i = 0; i < lights.length; i++) {
+    var soc = require('net').Socket();
+    soc.connect(5577, lights[i]);
+    devices.push(soc);
+  }
+
+  app.listen(port);
+  console.log('Listening at http://localhost:' + port);
+}
 
 function lightsOn() {
-  controller.turnOn(b1);
-  controller.turnOn(b2);
-  controller.turnOn(b3);
+  for(var i = 0; i < devices.length; i++) {
+    controller.turnOn(devices[i]);
+  }
 }
 
 function lightsOff() {
-  controller.turnOff(b1);
-  controller.turnOff(b2);
-  controller.turnOff(b3);
+  for(var i = 0; i < devices.length; i++) {
+    controller.turnOff(devices[i]);
+  }
 }
 
 app.get('/', (req,res) => {
@@ -53,5 +67,4 @@ app.post('/api/off/', (req,res) => {
   res.end('turning off lights');
 });
 
-app.listen(port);
-console.log('Listening at http://localhost:' + port);
+init();
